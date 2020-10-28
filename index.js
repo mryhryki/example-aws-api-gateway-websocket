f45fd16aa52c8267e6f362fd32ff5a86f617e4b0
hyiromori <hyiromori@gmail.com>
2020-10-29 08:14:47 +0900

    レスポンスにリクエストデータを含めるようにした
    どのリクエストに対するレスポンスなのか判断できる

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const {ApiGatewayManagementApi} = require('aws-sdk')
 
 const onMessage = async (event) => {
+  const { body } = event
   const {connectionId, apiId, stage} = event.requestContext;
   const endpoint = `https://${apiId}.execute-api.${process.env.AWS_REGION}.amazonaws.com/${stage}`
 
   const apiGateway = new ApiGatewayManagementApi({apiVersion: '2018-11-29', endpoint})
-  const params = {Data: JSON.stringify({endpoint, connectionId}), ConnectionId: connectionId}
+  const params = {Data: JSON.stringify({endpoint, connectionId, request: body}), ConnectionId: connectionId}
   await apiGateway.postToConnection(params).promise()
 
   return {}
